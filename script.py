2785d20b09e963f9cf52b406ba64668b063443b4
Lakshmi Mounika <thotalakshmimounika@gmail.com>
2023-08-16 18:48:53 +0530

    update files

--- script.py
+++ script.py
@@ -1,23 +1,20 @@
-import cv2
+import cv2,glob
  
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-cap = cv2.VideoCapture("elon.jpg")
+all_images=glob.glob("*.jpg")
+detect   = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+for image in all_images:
+        img = cv2.imread(image)
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = detect.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5)
  
-ret, img = cap.read()
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        for (x,y,w,h) in faces:
+                # To draw a rectangle in a face
+                final_img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
  
-# Detects faces of different sizes in the input image
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        cv2.imshow("Face Detection",final_img)
+        k = cv2.waitKey(2000)
+        
  
-for (x,y,w,h) in faces:
-        # To draw a rectangle in a face
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
- 
-cv2.imshow('img',img)
-k = cv2.waitKey(0)
- 
-# Close the window
-cap.release()
- 
-# De-allocate any associated memory usage
-cv2.destroyAllWindows()
+        # De-allocate any associated memory usage
+        cv2.destroyAllWindows()
